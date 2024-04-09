add6c183653696b2a358fb583642e264a4667d45
Johanna <johanna.werner@hof-university.de>
2024-04-10 00:07:34 +0200

    feat: inital commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get('http://localhost:8080').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={apiCall}>Make API Call</button>
+
       </header>
     </div>
   );
 }
-
-export default App;
