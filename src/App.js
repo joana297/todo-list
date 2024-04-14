b226bbd69f7d034a01bcfefaa65f146ca564f20f
Johanna <johanna.werner@hof-university.de>
2024-04-14 22:18:33 +0200

    test: API test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ListWrapper from "./components/ListWrapper";
-import ListItem from "./components/ListItem";
+import axios from 'axios';
 
 export default function App() {
   const addNewList = () => {
     console.log("neue Liste erstellt");
   }
 
+  function getHello() {
+    axios.get('http://localhost:8080/hello').then((res) => {
+      console.log(res.data);
+    });
+  }
+
   return (
     <>
       <header>
         <h1 className='align-center'>My To Do List</h1>
-        <i className="material-symbols-rounded">
+        <i className="material-symbols-rounded" onClick={getHello} >
           notifications
         </i>
       </header>
