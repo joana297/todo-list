8e951150fb5587b978ed6ae0d0fe4c8ed9796754
Johanna <johanna.werner@hof-university.de>
2024-05-13 00:31:18 +0200

    fix: edited styling

--- src/components/notifications/NotificationMessage.jsx
+++ src/components/notifications/NotificationMessage.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import style from './NotificationMessage.module.scss';
 import axios from 'axios';
 import url from '../../BackendURL';
+import { format, parseISO } from 'date-fns';
 
 export default function NotificationMessage(props) {
 
     const [list, setList] = useState({});
     const [todo, setTodo] = useState({});
     const [notification, setNotification] = useState({});
+    const [formatedDate, setFormatedDate] = useState("");
 
     useEffect(() => {
         axios.get(url + '/api/lists/' + props.notification.list_id).then((res) => {
@@ -17,12 +19,13 @@ export default function NotificationMessage(props) {
             setTodo(res.data[0]);
         });
         setNotification(props.notification);
+        setFormatedDate(format(parseISO(props.notification.date_time), "dd.MM.yyyy' - 'HH:mm"))
     }, [props.notification]);
 
     return (
         <div className={style.notification_wrapper}>
             <div className={style.infotext}>
-                <p>{notification.date_time}</p>
+                <p className={style.date}>{formatedDate}</p>
                 <h4>To Do: {todo.text}</h4>
                 <p>in Liste: {list.title}</p>
             </div>
