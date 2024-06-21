743822f048ebc6fecde207e71f291043c0a77eb8
Johanna <johanna.werner@hof-university.de>
2024-06-21 03:05:31 +0200

    feat: added caching

--- src/components/lists/ListItem.jsx
+++ src/components/lists/ListItem.jsx
@@ -4,26 +4,31 @@ import Notification from './Notification';
 import axios from 'axios';
 import url from '../../BackendURL';
 import { format } from 'date-fns';
-import { useNavigate } from 'react-router';
+import Swal from 'sweetalert2';
 
 export const NotificationContext = createContext(false);
 
 function ListItem(props) {
   const { updateNotifications, setUpdateNotifications } = useContext(NotificationContext);
-  const navigate = useNavigate();
 
   const [todo, setTodo] = useState({});
   const [todoText, setTodoText] = useState("");
   const [isDone, setIsDone] = useState(false);
+
+  const [allNotifications, setAllNotifications] = useState([]);
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
+    getAllNotifications();
+  }, []);
+
+  useEffect(() => {
     setTodo(props.todo);
+    setTodoText(props.todo.text);
+    setIsDone(props.todo.is_done);
   }, [props.todo]);
 
   useEffect(() => {
-    setTodoText(todo.text);
-    setIsDone(todo.is_done);
     getNotifications();
   }, [todo]);
 
@@ -35,18 +40,54 @@ function ListItem(props) {
   }, [updateNotifications]);
 
   /**
-    * updates the text & status of the todo
+    * gets all notifications & puts it in cache
     */
-  const updateTodo = async (done) => {
-    await axios.put(url + '/api/lists/' + todo.list_id + '/todos/' + todo.id,
-      {
-        text: todoText,
-        is_done: done
-      },
-      { headers: { 'Content-Type': 'application/json' } }).then(res => {
-        console.log(res);
-        props.update();
+  const getAllNotifications = () => {
+    axios.get(url + '/api/notifications')
+      .then(res => {
+        setAllNotifications(res.data.notifications);
+        localStorage.setItem('cachedNotifications', JSON.stringify(res.data.notifications));
+      })
+      .catch(error => {
+        console.log(error);
+        setAllNotifications(JSON.parse(localStorage.getItem('cachedNotifications')));
       });
+  }
+
+  /**
+     * gets all notifications for the todo from db or cache
+     */
+  const getNotifications = () => {
+    axios.get(url + '/api/lists/' + todo.list_id + '/todos/' + todo.id + '/notifications')
+      .then(res => {
+        setNotifications(res.data.notifications);
+      })
+      .catch(error => {
+        console.log(error);
+        setNotifications(allNotifications.filter(n => n.todo_id = todo.id));
+      });
+  }
+
+  /**
+     * validates if the text of the todo is empty
+     * @param {} e 
+     */
+  const validateText = (e) => {
+    if (e.target.value == null || e.target.value == "") {
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        iconColor: '#ff6a6a',
+        title: 'Feld darf nicht leer sein!',
+        showConfirmButton: true,
+        confirmButtonText: 'Ok',
+        confirmButtonColor: '#bef983',
+      }).then(() => {
+        updateTodo(isDone, "My Todo");
+      });
+    } else {
+      updateTodo(isDone);
+    }
   }
 
   /**
@@ -58,27 +99,28 @@ function ListItem(props) {
   }
 
   /**
-     * gets all notifications for the todo item from db
-     */
-  const getNotifications = () => {
-    axios.get(url + '/api/lists/' + todo.list_id + '/todos/' + todo.id + '/notifications')
+    * updates the text & status of the todo
+    */
+  const updateTodo = async (done, text) => {
+    await axios.put(url + '/api/lists/' + todo.list_id + '/todos/' + todo.id,
+      {
+        text: text ?? todoText,
+        is_done: done
+      }, { headers: { 'Content-Type': 'application/json' } })
       .then(res => {
-        setNotifications(res.data.notifications);
+        console.log(res);
+        props.update();
       })
       .catch(error => {
-        console.error('Error fetching the notifications:', error);
-        navigate('/404');
-      });
-  }
-
-  /**
-  * deletes a notification from the todo item by its id
-  */
-  const deleteNotification = async (notification) => {
-    await axios.delete(url + '/api/lists/' + notification.list_id + '/todos/' + notification.todo_id + '/notifications/' + notification.id)
-      .then((res) => {
-        console.log(res.data);
-        getNotifications();
+        console.log(error);
+        var allTodos = JSON.parse(localStorage.getItem('cachedTodos'));
+        var updatedItem = { text: text ?? todoText, is_done: done }
+        allTodos.map(item => {
+          if (item.id == todo.id) {
+            item = updatedItem
+          }
+        });
+        localStorage.setItem('cachedLists', JSON.stringify(allTodos));
       });
   }
 
@@ -89,11 +131,41 @@ function ListItem(props) {
     await axios.post(url + '/api/lists/' + todo.list_id + '/todos/' + todo.id + '/notifications',
       {
         date_time: format(new Date(), "yyyy-MM-dd'T'HH:mm")
-      },
-      { headers: { 'Content-Type': 'application/json' } }).then(res => {
-        console.log(res);
+      }, { headers: { 'Content-Type': 'application/json' } })
+      .then(res => {
+        console.log(res.data.notification);
         getNotifications();
+
+        allNotifications.push(res.data.notification);
+        localStorage.setItem('cachedNotifications', JSON.stringify(allNotifications));
         setUpdateNotifications(true);
+      })
+      .catch(error => {
+        console.log(error);
+        var data = {
+          id: allNotifications[allNotifications.length - 1].id + 1,
+          list_id: todo.list_id,
+          todo_id: todo.id,
+          date_time: format(new Date(), "yyyy-MM-dd'T'HH:mm")
+        }
+        allNotifications.push(data);
+        localStorage.setItem('cachedNotifications', JSON.stringify(allNotifications));
+      });
+  }
+
+  /**
+  * deletes a notification from the todo item by its id
+  */
+  const deleteNotification = async (notification) => {
+    await axios.delete(url + '/api/lists/' + notification.list_id + '/todos/' + notification.todo_id + '/notifications/' + notification.id)
+      .then((res) => {
+        getNotifications();
+      })
+      .catch(error => {
+        console.log(error);
+        var remList = allNotifications.filter(n => n.id != notification.id);
+        localStorage.setItem('cachedNotifications', JSON.stringify(remList));
+        getNotifications();
       });
   }
 
@@ -106,9 +178,9 @@ function ListItem(props) {
 
         <div className={style.title}>
           <input type='text'
-            value={todoText ?? ""}
+            value={todoText ?? "Todo"}
             onChange={(e) => setTodoText(e.target.value)}
-            onBlur={() => { updateTodo(isDone) }} />
+            onBlur={validateText} />
           <span className={style.line} />
         </div>
 
